9c39e67eb4b296527fa7b72ce077f8932ce6c962
rijalazani <rijal.azani@gmail.com>
2018-12-04 16:20:12 +0700

    slider
    update slider di home

--- app/src/main/java/tukangdagang/id/co/tukangdagang_koperasi/Home.java
+++ app/src/main/java/tukangdagang/id/co/tukangdagang_koperasi/Home.java
@@ -2,6 +2,8 @@ package tukangdagang.id.co.tukangdagang_koperasi;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +17,15 @@ import android.support.v7.widget.GridLayout;
 import android.view.View;
 //import android.widget.GridLayout;
 //import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 //
 //import id.tukangdagang.com.tukangdagangdevel._sliders.FragmentSlider;
 //import id.tukangdagang.com.tukangdagangdevel._sliders.SliderIndicator;
@@ -39,6 +44,11 @@ public class Home extends Fragment {
 //    private SliderView sliderView;
 //    private LinearLayout mLinearLayout;
     GridLayout mainGrid;
+
+    ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
 
 
     public Home() {
@@ -60,7 +70,86 @@ public class Home extends Fragment {
 //        sliderView = (SliderView) rootView.findViewById(R.id.sliderView);
 //        mLinearLayout = (LinearLayout) rootView.findViewById(R.id.pagesContainer);
 //        setupSlider();
+
+
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
+
+        sliderDotspanel = (LinearLayout) rootView.findViewById(R.id.SliderDots);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getContext());
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        dotscount = viewPagerAdapter.getCount();
+        dots = new ImageView[dotscount];
+
+        for(int i = 0; i < dotscount; i++){
+
+            dots[i] = new ImageView(getContext());
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+
+            sliderDotspanel.addView(dots[i], params);
+
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.nonactive_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(), 2000, 4000);
+
+
         return rootView;
+    }
+
+    public class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            if(getActivity() == null)
+                return;
+            getActivity().runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    if(viewPager.getCurrentItem() == 0){
+                        viewPager.setCurrentItem(1);
+                    } else if(viewPager.getCurrentItem() == 1){
+                        viewPager.setCurrentItem(2);
+                    } else {
+                        viewPager.setCurrentItem(0);
+                    }
+
+                }
+            });
+
+        }
     }
     //    private void setupSlider() {
 //        sliderView.setDurationScroll(800);
