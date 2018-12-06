76f774c5fcbc691f245287c89d6cc7b01ccd3b8e
rijalazani <rijal.azani@gmail.com>
2018-12-07 02:19:32 +0700

    login
    desain layout login

---
+++ app/src/main/java/tukangdagang/id/co/tukangdagang_koperasi/Login.java
@@ -0,0 +1,27 @@
+package tukangdagang.id.co.tukangdagang_koperasi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+private Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnlogin = findViewById(R.id.btnLogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
