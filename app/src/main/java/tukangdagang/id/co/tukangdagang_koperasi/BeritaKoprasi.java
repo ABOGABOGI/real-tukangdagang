31883f1b6c7f514d5dab0b943c4afa8cd5ce8cd8
rijalazani <rijal.azani@gmail.com>
2019-02-04 04:26:45 +0700

    Update BeritaKoprasi.java
    menampilkan daftar anggota yang sudah mendaftar di salah satu koperasi

--- app/src/main/java/tukangdagang/id/co/tukangdagang_koperasi/BeritaKoprasi.java
+++ app/src/main/java/tukangdagang/id/co/tukangdagang_koperasi/BeritaKoprasi.java
@@ -175,40 +175,6 @@ public class BeritaKoprasi extends AppCompatActivity implements SwipeRefreshLayo
 
                             initRecyclerView();
 
-//
-//
-//        mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
-//        mNames.add("Havasu Falls");
-//
-//        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-//        mNames.add("Trondheim");
-//
-//        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-//        mNames.add("Portugal");
-//
-//        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-//        mNames.add("Rocky Mountain National Park");
-//
-//
-//        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-//        mNames.add("Mahahual");
-//
-//        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
-//        mNames.add("Frozen Lake");
-//
-//
-//        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
-//        mNames.add("White Sands Desert");
-//
-//        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
-//        mNames.add("Austrailia");
-//
-//        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
-//        mNames.add("Washington");
-//
-//        initRecyclerView();
-
-
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
@@ -230,8 +196,7 @@ public class BeritaKoprasi extends AppCompatActivity implements SwipeRefreshLayo
                     };
 
                     RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
-
+                    requestQueue.add(stringRequest);
 
                 }
 
@@ -255,6 +220,5 @@ public class BeritaKoprasi extends AppCompatActivity implements SwipeRefreshLayo
     @Override
     public void onRefresh() {
         getdata();
-        getImages();
     }
 }
